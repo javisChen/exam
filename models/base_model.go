ee61f7be615accad27b59890ca3e644b74b27216
Javis <javischen9548@foxmail.com>
2021-09-22 18:52:01 +0800

    feat:添加新增试卷接口

--- models/base_model.go
+++ models/base_model.go
@@ -4,7 +4,7 @@ import "time"
 
 type BaseModel struct {
 	Id          uint64    `json:"id,omitempty"`
-	GmtCreated  time.Time `json:"gmt_created" orm:"-"`
-	GmtModified time.Time `json:"gmt_modified" orm:"-"`
-	IsDeleted   uint      `json:"is_deleted,omitempty" orm:"-"`
+	GmtCreated  time.Time `json:"gmt_created"`
+	GmtModified time.Time `json:"gmt_modified"`
+	IsDeleted   uint      `json:"is_deleted,omitempty"`
 }
